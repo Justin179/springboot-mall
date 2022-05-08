1becf363f87f92d4c422ccc0757a34acdc76785d
陳君豪 <justin.uob@gmail.com>
2022-05-08 10:56:50 +0800

    商品功能 - 實作查詢商品列表 (模糊查詢)

--- src/main/java/com/example/springbootmall/dao/impl/ProductDaoImpl.java
+++ src/main/java/com/example/springbootmall/dao/impl/ProductDaoImpl.java
@@ -1,5 +1,6 @@
 package com.example.springbootmall.dao.impl;
 
+import com.example.springbootmall.constant.ProductCategory;
 import com.example.springbootmall.dao.ProductDao;
 import com.example.springbootmall.dto.ProductRequest;
 import com.example.springbootmall.model.Product;
@@ -88,11 +89,20 @@ public class ProductDaoImpl implements ProductDao {
     }
 
     @Override
-    public List<Product> getProducts() {
+    public List<Product> getProducts(ProductCategory category, String search) {
         String sql = "select product_id, product_name, category, image_url, price, stock, description, created_date, " +
-                "last_modified_date from product";
+                "last_modified_date from product where 1=1";
 
         Map<String ,Object> map = new HashMap<>();
+
+        if (category!=null){
+            sql = sql + " and category = :category";
+            map.put("category",category.name());
+        }
+        if (search !=null){
+            sql = sql + " and product_name like :search";
+            map.put("search","%"+search+"%");
+        }
 
         List<Product> productList = namedParameterJdbcTemplate.query(sql, map, new ProductRowMapper());
 
