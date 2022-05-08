ce972457443e959cc0731ac062372ed06f6d58e9
陳君豪 <justin.uob@gmail.com>
2022-05-08 14:49:03 +0800

    商品功能 - 實作查詢商品列表 (分頁)

--- src/main/java/com/example/springbootmall/controller/ProductController.java
+++ src/main/java/com/example/springbootmall/controller/ProductController.java
@@ -8,11 +8,15 @@ import com.example.springbootmall.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import java.util.List;
 
+@Validated // 這個要加，Max & Min才會生效
 @RestController
 public class ProductController {
 
@@ -26,13 +30,19 @@ public class ProductController {
             @RequestParam(required = false) String search,
             // sorting
             @RequestParam(defaultValue = "created_date") String orderBy,
-            @RequestParam(defaultValue = "desc") String sort){
+            @RequestParam(defaultValue = "desc") String sort,
+            // pagination
+            @RequestParam(defaultValue = "5") @Max(1000) @Min(0) Integer limit, // 最多查幾筆
+            @RequestParam(defaultValue = "0") @Min(0) Integer offset // 要跳過的筆數
+            ){
 
         ProductQueryParams productQueryParams = new ProductQueryParams();
         productQueryParams.setCategory(category);
         productQueryParams.setSearch(search);
         productQueryParams.setOrderBy(orderBy);
         productQueryParams.setSort(sort);
+        productQueryParams.setLimit(limit);
+        productQueryParams.setOffset(offset);
 
         List<Product> productList = productService.getProducts(productQueryParams);
         return ResponseEntity.status(HttpStatus.OK).body(productList);
--- src/main/java/com/example/springbootmall/dto/ProductQueryParams.java
+++ src/main/java/com/example/springbootmall/dto/ProductQueryParams.java
@@ -3,7 +3,28 @@ package com.example.springbootmall.dto;
 import com.example.springbootmall.constant.ProductCategory;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.validation.constraints.Min;
+
 public class ProductQueryParams {
+
+    public Integer getLimit() {
+        return limit;
+    }
+
+    public void setLimit(Integer limit) {
+        this.limit = limit;
+    }
+
+    public Integer getOffset() {
+        return offset;
+    }
+
+    public void setOffset(Integer offset) {
+        this.offset = offset;
+    }
+
+    private Integer limit; // 最多查幾筆
+    private Integer offset;
 
     public String getOrderBy() {
         return orderBy;
